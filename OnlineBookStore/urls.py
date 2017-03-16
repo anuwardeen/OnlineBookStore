fb47451b59bf21a54ce90eeff2a1d1c76e561471
monish7108 <monish.lalchandani@tarams.com>
2017-03-16 17:36:28 +0530

    second commit

--- OnlineBookStore/urls.py
+++ OnlineBookStore/urls.py
@@ -6,16 +6,4 @@ urlpatterns = [
 
     url(r'^top-authors-and-their-works/rest/$', views.top_authors_and_works_rest, name="top-authors-and-their-works-rest"),
 
-    # url(r'^top-authors-and-works/$',views.top_authors_and_works, "top-authors-and-their-works"),
-    # url(r'^authors/rest/$', views.authors_list_rest, name="authorList"),
-    # url(r'^top-authors-and-their-works/$', views.top_authors_and_works, name="topTenBooks"),
-    # url(r'^books/rest/$', views.books_list_rest, name="bookList"),
-    # url(r'^books-of-author/rest/(?P<author_id>\d+)$', views.authors_work_rest, name="authorsWorks"),
-    # url(r'^author-of-the-year/rest/(?P<year>\d+)', views.author_of_the_year_rest, name="authorOfTheYear"),
-    #
-    # url(r'^authors/$', views.authors_list, name="authorList"),
-    # url(r'^books/$', views.books_list, name="bookList"),
-    # url(r'^books-of-author/(?P<author_id>\d+)$', views.authors_work, name="authorsWorks"),
-    # url(r'^top-ten-books/$', views.top_ten_books_rest, name="topTenBooks"),
-    # url(r'^author-of-the-year/(?P<year>\d+)', views.author_of_the_year_rest, name="authorOfTheYear"),
 ]
